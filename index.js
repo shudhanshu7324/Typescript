bb5af89783a0fc874cb9d836ce004bf11c1fb340
Shudhanshu Shree <shudhanshu7324@gmail.com>
2025-01-15 16:55:02 +0530

    interface in ts

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
 // const a:number = 1;
 // const b:number = 2;
-var p1 = {
-    name: 'John',
-    age: 25
+var bankAccount = {
+    accountNumber: 123456,
+    accountHolderName: 'John Doe',
+    balance: 1000,
+    isActive: true,
+    transactions: [
+        {
+            PayerAccount: 123456,
+            PayeeAccount: 654321
+        },
+        {
+            PayerAccount: 123456,
+            PayeeAccount: 43223
+        }
+    ]
 };
-var p2 = {
-    name: 'Jane',
-    age: 22,
-    address: 'New York'
+var book1 = {
+    name: "Ncert",
+    price: 1099,
 };
-console.log(p1.name);
-console.log(p1 === null || p1 === void 0 ? void 0 : p1.address);
+var ebook1 = {
+    name: "Maths",
+    price: 459,
+    size: 453,
+    type: "PDF"
+};
+var audioBook1 = {
+    name: "Science",
+    price: 999,
+    duration: 120
+};
+console.log(audioBook1.name);
+console.log(audioBook1.price);
+console.log(audioBook1.duration);
+var user = {
+    name: "John",
+    age: 25,
+    address: "New York"
+};
+console.log(user.name);
+console.log(user.age);
+console.log(user.address);
+function printId(id) {
+    console.log(id);
+    console.log(typeof id);
+}
+printId("10");
--- index.ts
+++ index.ts
@@ -20,22 +20,157 @@
 
 // Types Alias
 
-type Person = {
-    name: string;
-    age: number;
-    address?: string;
+// type Person = {
+//     name: string;
+//     age: number;
+//     address?: string;
+// }
+
+// const p1:Person = {
+//     name: 'John',
+//     age: 25
+// }
+
+// const p2:Person = {
+//     name: 'Jane',
+//     age: 22,
+//     address: 'New York'
+// }
+
+// console.log(p1.name)
+// console.log(p1.address)
+
+
+// Interface -> Interface is a way to define a type in TypeScript or a contract that a class should follow. it is a shape of an object.
+
+
+// Transaction Interface
+interface Transaction {
+    PayerAccount: number;
+    PayeeAccount: number;
 }
 
-const p1:Person = {
-    name: 'John',
-    age: 25
+interface BankAccount{
+    accountNumber:number;
+    accountHolderName:string;
+    balance:number;
+    isActive:boolean;
+    transactions:Transaction[];
 }
 
-const p2:Person = {
-    name: 'Jane',
-    age: 22,
-    address: 'New York'
+const bankAccount:BankAccount = {
+    accountNumber: 123456,
+    accountHolderName: 'John Doe',
+    balance: 1000,
+    isActive: true,
+    transactions: [
+        {
+            PayerAccount: 123456,
+            PayeeAccount: 654321
+        },
+        {
+            PayerAccount: 123456,
+            PayeeAccount: 43223
+        }
+    ]
 }
 
-console.log(p1.name)
-console.log(p1.address)
+
+// Extend Interface
+interface Book{
+    name:string;
+    price:number;
+}
+
+const book1 : Book = {
+    name:"Ncert",
+    price: 1099,
+}
+
+
+interface Ebook{
+    name:string;
+    price:number;
+    size:number;
+    type:string;
+}
+
+const ebook1:Ebook = {
+    name:"Maths",
+    price: 459,
+    size: 453,
+    type: "PDF"
+}
+
+// Do not repeat the same code again and again so used extends keyword works like inheritance
+interface AudioBook extends Book{
+    duration:number;
+}
+
+const audioBook1:AudioBook = {
+    name: "Science",
+    price: 999,
+    duration: 120
+}
+
+console.log(audioBook1.name)
+console.log(audioBook1.price)
+console.log(audioBook1.duration)
+
+
+// If there is two interface with same name then it will merge the properties of both interface
+
+interface User{
+    name:string;
+    age:number;
+}
+
+interface User{
+    address:string;
+}
+
+const user:User = {
+    name: "John",
+    age: 25,
+    address: "New York"
+}
+
+console.log(user.name)
+console.log(user.age)
+console.log(user.address)
+
+
+// Some important points about Interface and Type Alias
+// merging of interface is possible but merging of type alias is not possible
+
+// type User = {
+//     name:string;
+//     age:number;
+// }
+
+// type User = {
+//     address:string;
+// } 
+// this will give error
+
+// Limitation of Interface
+// type sanitizedString = string;
+// type EvenNumber = number;
+
+// it cannot be done 
+// interface sanitizedString extends string{
+
+// }
+
+
+
+// Union Type
+// type ID = string | number;
+// function printId(id:ID){
+//     console.log(id)
+//     console.log(typeof id)
+// }
+
+// printId("10")
+
+
